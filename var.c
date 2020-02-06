70a22bb97d664f70c9f036de3e92c5ebf522a2ae
Akshara P <aksharap.181it132@nitk.edu.in>
2020-02-06 15:04:10 +0530

    Added basic lexical analyzer

---
+++ var.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a , b, c;
+a=1; //this int is in comment
+b=2;
+c=a + b;
+char x[5];
+int i;
+for  (i=0;i<5;i++ )
+{
+    int a;
+    scanf("%c",&x[i]); 
+
+while(i<10)
+{
+    printf("d");
+    i++;
+}
+printf("Sum:%d",c);
+
+}}
